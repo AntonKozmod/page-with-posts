1ed528b82337ace73845abd6816e781d76d80e14
aikozmod <antoxa.kozmod@gmail.com>
2021-09-21 23:01:32 +0300

    Пагинация, дабавлен постраничный вывод

--- src/app.vue
+++ src/app.vue
@@ -27,6 +27,19 @@
             v-if="!isPostsLoading"
         />
         <div v-else>Идет загрузка...</div>
+        <div class="page__wrapper">
+            <div 
+                v-for="pageNumber in totalPages" 
+                :key="pageNumber"
+                class="page"
+                :class="{
+                    'current-page': page === pageNumber
+                }"
+                @click="changePage(pageNumber)"
+            >
+                {{ pageNumber }}
+            </div>
+        </div>
     </div>
 </template>
     
@@ -47,6 +60,9 @@
                 isPostsLoading: false,
                 selectedSort: '',
                 searchQuery: '',
+                page: 1,
+                limit: 10,
+                totalPages: 0,
                 sortOptions: [
                     {value: 'title', name: 'По названию'},
                     {value: 'body', name: 'По описанию'},
@@ -64,10 +80,19 @@
             showDialog() {
                 this.dialogVisible = true
             },
+            changePage(pageNumber) {
+                this.page = pageNumber;
+            },
             async fetchPosts() {
                 try {
                     this.isPostsLoading = true
-                    const response = await axios.get('https://jsonplaceholder.typicode.com/posts?_limit=10')
+                    const response = await axios.get('https://jsonplaceholder.typicode.com/posts', {
+                        params: {
+                            _page: this.page,
+                            _limit: this.limit
+                        }
+                    });
+                    this.totalPages = Math.ceil(response.headers['x-total-count'] / this.limit)
                     this.posts = response.data
                     this.isPostsLoading = false
                 } catch (e) {
@@ -88,14 +113,11 @@
                 return this.sortedPosts.filter(post => post.title.toLowerCase().includes(this.searchQuery.toLowerCase()))
             }
         },
-        // watch: {
-        //     selectedSort(newValue) {
-        //         // newValue === this.selectedSort
-        //         this.posts.sort((post1, post2) => {
-        //             return post1[newValue]?.localeCompare(post2[newValue])
-        //         })
-        //     },
-        // }
+        watch: {
+            page() {
+                this.fetchPosts();
+            }        
+        }
     }
 </script>
 
@@ -116,4 +138,18 @@
     margin: 15px 0;
 }
 
+.page__wrapper {
+    display: flex;
+    margin-top: 15px;
+}
+
+.page {
+    border: 1px solid black;
+    padding: 10px;
+
+}
+
+.current-page {
+    border: 2px solid green;
+}
 </style>
