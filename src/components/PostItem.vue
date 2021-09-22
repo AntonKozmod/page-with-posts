69ab81f2a9460b634670722b0584a1ca24489853
aikozmod <antoxa.kozmod@gmail.com>
2021-09-22 22:20:17 +0300

    Добавлена динамическая навигация

--- src/components/PostItem.vue
+++ src/components/PostItem.vue
@@ -6,6 +6,11 @@
         </div>
         <div class="post__btns">
             <my-button
+                @click="$router.push(`/posts/${post.id}`)"
+            >
+                Открыть
+            </my-button>
+            <my-button
                 @click="$emit('remove', post)"
             >
                 Удалить
@@ -33,5 +38,8 @@ export default {
         display: flex;
         align-items: center;
         justify-content: space-between;
+    }
+    .post__btns {
+        display: flex;
     }
 </style>
